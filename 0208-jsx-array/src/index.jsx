7b036b1c176fd15208434db09f70fbdcbb19c30b
JoaoXavierWin <joao.xavier21@gmail.com>
2024-07-25 15:54:17 -0300

    exercicios

---
+++ 0208-jsx-array/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+import Exercicio from './exercicio.jsx'
+
+//ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Exercicio />)
